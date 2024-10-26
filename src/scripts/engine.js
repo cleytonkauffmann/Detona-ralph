90bb7c7badb63db08677c518c869ca05b86d5d91
cleytonkauffmann <165606386+cleytonkauffmann@users.noreply.github.com>
2024-10-25 22:14:18 -0300

    first commit

---
+++ src/scripts/engine.js
@@ -0,0 +1,8 @@
+const state = {
+    view:{
+        squares: document.querySelectorAll(".square"),
+        enemy: document.querySelector(".enemy"),
+        time-left-number: document.querySelector("#time-left-number"),
+        time-score-number: document.querySelector("#time-score-number"),
+    },
+    values:{}
